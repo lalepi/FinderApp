477fdd37f2889939383fff4b3f904a2bd53af7a3
AG2889 <AG2889@student.jamk.fi>
2025-01-26 10:23:37 +0200

    Added axios and json server packages. made quick test to App.tsx to show testdataproducts. Json server is for making the frontend development work easier now until we have real db in action. Check the package.json to run the server.

--- pff-frontend/src/App.tsx
+++ pff-frontend/src/App.tsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from 'react'
+import productsService from './services/Products'
+
 function App() {
+    const [productList, setProductList] = useState([])
+
+    useEffect(() => {
+        productsService.getAll().then((products) => {
+            console.log(products)
+            setProductList(
+                products.map((product) => (
+                    <div key={product.id}>
+                        <h2>{product.name}</h2>
+
+                        <li>{product.size}</li>
+                        <li>{product.manufacturer}</li>
+                        <li>{product.ingredients}</li>
+                        <li>{product.age}</li>
+                    </div>
+                ))
+            )
+        })
+    }, [])
+
     return (
         <>
             <div>
                 <h1>PetFoodFinder</h1>
+                <h2>Products</h2>
+                {productList}
             </div>
         </>
     )
